6ae5e39917bd2543f72619b9501575e4a4c0d777
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-14 09:46:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,8 +7,8 @@ def turn(board)
   
   if valid_move?(board, index) == false
     puts "Please enter 1-9:"
-    input = gets.strip
-    index = input_to_index(input)
+    # input = gets.strip
+    # index = input_to_index(input)
   end
 end
 
